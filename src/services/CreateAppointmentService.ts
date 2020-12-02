f362ddb57ddc5e858d5d550710ca7774fda651ee
joao911 <joaopaulolacerda911@gmail.com>
2020-12-02 10:22:39 -0300

    table funcionando

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,33 +1,32 @@
 import Appointment from "../models/Appointment";
-import {startOfHour} from 'date-fns'
-import AppointmentRepository from '../repositories/AppointmentsRepository';
+import { startOfHour } from "date-fns";
+import { getCustomRepository } from "typeorm";
+import AppointmentRepository from "../repositories/AppointmentsRepository";
+
 
 interface Request {
   provider: string;
   date: Date;
 }
 class CreateAppointmentService {
-    private appointmentsRepository : AppointmentRepository;
-
-  constructor(appointmentsRepository: AppointmentRepository) {
-     this.appointmentsRepository =appointmentsRepository;
-  }
-
-  public execute({ date, provider }: Request): Appointment {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentRepository);
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
-        appointmentDate
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate
     );
 
     if (findAppointmentInSameDate) {
       throw Error("this appointment is already booked");
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+    await appointmentsRepository.save(appointment);
+
     return appointment;
   }
 }
