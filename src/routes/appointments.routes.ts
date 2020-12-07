5cce669a38ee026d808cef6144f371ca2b6879db
joao911 <joaopaulolacerda911@gmail.com>
2020-12-07 14:56:27 -0300

    rota de autenticação com defeito

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,11 +3,13 @@ import { parseISO } from "date-fns";
 import {getCustomRepository} from 'typeorm';
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 import CreateAppointmentService from "../services/CreateAppointmentService";
+import ensureAuthenticaded from '../midlewares/ensureAuthenticaded';
 
 const appointmentsRouter = Router();
-
+appointmentsRouter.use(ensureAuthenticaded);
 
 appointmentsRouter.get("/", async (request, response) => {
+  
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments =  await appointmentsRepository.find();
   return response.json(appointments);
